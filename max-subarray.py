12cbea1ee76df5ef87444c2896909dff1f58f633
Witold Zolnowski <blue3656@gmail.com>
2024-01-24 15:35:37 +0000

    max-subarray - brute force - fail

--- max-subarray.py
+++ max-subarray.py
@@ -1,1 +1,22 @@
 # https://leetcode.com/problems/maximum-subarray/description/
+
+from typing import List
+
+
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max = 0
+        max_i = None
+        # brute force 
+        for ix, x in enumerate(nums):
+            for iy, y in enumerate(nums[ix:]):
+                temp = 0
+                for q in nums[ix:iy+1]:
+                    temp += q
+                    print(f"subarray is {nums[ix:iy+1]}")
+                if temp > max:
+                    max = temp
+        print(max)
+
+Solution().maxSubArray(nums)
