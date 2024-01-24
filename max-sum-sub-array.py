2d2d5998b42173d35655d99cb03b0c2c61afbe2b
Witold Zolnowski <blue3656@gmail.com>
2024-01-24 11:08:39 +0000

    add todo

--- max-sum-sub-array.py
+++ max-sum-sub-array.py
@@ -3,7 +3,21 @@ from typing import List
 nums = [31,25,72,79,74]
 class Solution:
     def maxSum(self, nums: List[int]) -> int:
-        return 0
-
+        solution = -1
+        for i in nums:
+            for j in nums:
+                if i == j:
+                    continue
+                i_s = str(i)
+                j_s = str(j)
+                i_set = {x for x in i_s}
+                j_set = {y for y in j_s}
+                max_i = max(i_set)
+                max_j = max(j_set)
+                if max_i == max_j:
+                    sum = i + j
+                    if sum > solution:
+                        solution = sum
+        return solution
 
 print(Solution().maxSum(nums))
---
+++ todo.md
@@ -0,0 +1,7 @@
+1) max-pair-sum-in-an-array
+# https://leetcode.com/problems/max-pair-sum-in-an-array/solutions/3902240/using-map-detailed-approach-and-easy-to-understand-code/
+- solve using brute force
+- optimize using hash maps
+2) max sum sub array 
+- find a problem on leet code or in wild
+- solve it using divide and conquer method
