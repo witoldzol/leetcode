310e81e1ef2e97f434e601af14a2ae01579b9c80
Witold Zolnowski <blue3656@gmail.com>
2024-01-24 11:11:19 +0000

    brute force - done

--- max-sum-sub-array.py
+++ max-sum-sub-array.py
@@ -1,12 +1,13 @@
 # https://leetcode.com/problems/max-pair-sum-in-an-array/solutions/3902240/using-map-detailed-approach-and-easy-to-understand-code/
 from typing import List
 nums = [31,25,72,79,74]
+nums = [8,75,28,35,21,13,21]
 class Solution:
     def maxSum(self, nums: List[int]) -> int:
         solution = -1
-        for i in nums:
-            for j in nums:
-                if i == j:
+        for i_idx, i in enumerate(nums):
+            for j_idx, j in enumerate(nums):
+                if i_idx == j_idx:
                     continue
                 i_s = str(i)
                 j_s = str(j)
