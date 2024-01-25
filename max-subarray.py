f7078877006a2584570697e1287480ddc1ae007f
Witold Zolnowski <blue3656@gmail.com>
2024-01-25 06:22:58 +0000

    max-subarray brute force working

--- max-subarray.py
+++ max-subarray.py
@@ -7,16 +7,18 @@ nums = [-2,1,-3,4,-1,2,1,-5,4]
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         max = 0
-        max_i = None
-        # brute force 
+        maxi = None
         for ix, x in enumerate(nums):
-            for iy, y in enumerate(nums[ix:]):
-                temp = 0
-                for q in nums[ix:iy+1]:
-                    temp += q
-                    print(f"subarray is {nums[ix:iy+1]}")
-                if temp > max:
-                    max = temp
-        print(max)
+            print(f"====> i is {x}")
+            for iy, y in enumerate(nums[ix+1:]):
+                sum = 0
+                for z in nums[ix+1:-iy]:
+                    print(f"z is {z}")
+                    sum += z
+                print("==========")
+                if sum > max:
+                    max = sum
+                    maxi = (ix,iy)
+        print(f"Max sub is {max}, index {maxi}")
 
 Solution().maxSubArray(nums)
--- todo.md
+++ todo.md
@@ -1,7 +1,13 @@
 1) max-pair-sum-in-an-array
 # https://leetcode.com/problems/max-pair-sum-in-an-array/solutions/3902240/using-map-detailed-approach-and-easy-to-understand-code/
 - [x] solve using brute force
-- optimize using hash maps
+- [x] optimize using hash maps
 2) max sum sub array 
-- find a problem on leet code or in wild
+- [x] find a problem on leet code or in wild
+- [x] brute force
+[Issues encountered:
+I thought that it was a n^2 problem, but really, it is n^3 ( with some adjustments because each following iteration is smaller ) 
+Then I didn't realise that numbers have to be consequtive, so when defining ranges, make sure that the range closes in from both left
+and right side, example 1,2,3,4 => i would select 1, and then check 2,3,4|| 3,4 || 4 => what I needed to do was to check 2,3,4| 2,3 | 2
+which means narrowing the range from the "right"|end side of the array, not left]
 - solve it using divide and conquer method
