aa6769a3d36001e75747c817cc28c284aadd93df
Witold Zolnowski <blue3656@gmail.com>
2024-01-25 17:59:08 +0000

    temp

--- maxsubarray.py
+++ maxsubarray.py
@@ -31,53 +31,49 @@ class Solution:
                     maximum = sum
         return maximum
 
-    def merge(self, left: List[int], right: List[int]):
-        return left + right
-
-    def split(self, nums: List[int], m: int) -> Tuple[List[int], int]:
-        if len(nums) < 2:
-            # print(f"got to the bottom, {nums=}")
-            s = sum(nums)
-            if s > m:
-                m = s
-            return nums, m
-        middle = len(nums) // 2
-        left = nums[:middle]
-        l_s = sum(left)
-        right = nums[middle:]
-        r_s = sum(right)
-        lmm = len(left) // 2
-        rmm = len(right) // 2
-        ll = left[lmm:]
-        rr = right[:rmm]
-        llrr = sum(ll + rr)
-        # alternative
-        if len(left) % 2 != 0 and len(left) != 1:
-            ll = left[lmm - 1 :]
-        if len(right) % 2 != 0 and len(right) != 1:
-            rr = right[: rmm + 1]
-        alternative_llrr = sum(ll + rr)
-        print(f"middle is = {ll + rr}")
-        if llrr > m:
-            m = llrr
-        if alternative_llrr > m:
-            m = alternative_llrr
-        ls, lm = self.split(left, m)
-        rs, rm = self.split(right, m)
-        if l_s > m:
-            m = l_s
-        if r_s > m:
-            m = r_s
-        if lm > m:
-            m = lm
-        if rm > m:
-            m = rm
-        # print(f"{llrr=}, {l_s=}, {r_s=}, {lm=}, {rm=}")
-        return (ls + rs), m
-
-    def maxSubArray(self, nums: List[int]) -> int:
-        # divide and conquer
-        return self.split(nums, 0)[1]
-
-
-Solution().maxSubArray(nums)
+    def brute_force_with_pointers(self, nums: List[int]):
+    # if we find a negative number, continue
+    # if we find positive, save index as first_pointer, keep track of sum
+    # if we find subsequent negative number, keep going, update sum
+    # if we find a positive number, and it is bigger than current sum, update first_pointer to this index
+    # if the positive number is smaller, keep going, update sum
+    # at any point, check if sum > maximum. if yes, save index in second_pointer
+    #
+    # you should be done with a single pass
+        start_index = 0
+        max_index_start = 0
+        max_index_end = 0
+        max_sum = 0
+        sum = 0
+        # initiate start index
+        for i, x in enumerate(nums):
+            if x < 1:
+                continue
+            else:
+                start_index = i
+                max_index_start = i
+                sum = x
+                max_sum = x
+                break
+        # meat and potatoes 
+        for ix, x in enumerate(nums):
+            if x == 0:
+                continue
+            if x < 0:
+                sum += x
+            else:
+                # check if current value beats the current sum - in case previous value was negative
+                if x > sum:
+                    # set new max if x can beat it
+                    if x > max_sum:
+                        max_sum = x
+                        max_index_start = ix
+                    # set a new start pointer
+                    sum = x
+                    start_index = ix
+                else:
+                    sum += x
+                    if sum > max_sum:
+                        max_sum = sum
+                        max_index_end = ix
+        return max_sum
--- test.py
+++ test.py
@@ -26,6 +26,15 @@ def test_brute_force_cache(s, nums, expected):
     actual = s.brute_force_cache(nums)
     assert expected == actual
 
+@pytest.mark.parametrize("nums, expected",[
+    ([-1, 1, 2, -2], 3),
+    ([-2, -1, 1, 2, -2], 3),
+    ([-2, 1, 1, 2, -2], 4),
+    ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
+])
+def test_brute_force_with_pointers(s, nums, expected):
+    actual = s.brute_force_with_pointers(nums)
+    assert expected == actual
 # def test_divide_and_conquer(s):
 #
 #     nums = [-1, 1, 2, -2]
