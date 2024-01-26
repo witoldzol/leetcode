0c2328abb60742d66fe516063674421d3a1288f2
Witold Zolnowski <blue3656@gmail.com>
2024-01-26 07:56:10 +0000

    o(n) solution - mine, and from youtube

--- maxsubarray.py
+++ maxsubarray.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/maximum-subarray/description/
 
 from typing import List, Tuple
+from sys import maxsize
 
 nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 nums = [1, -1, 2, 1]
@@ -8,7 +9,6 @@ nums = [-1, 3, 2, -1]
 
 
 class Solution:
-
     def brute_force(self, nums: List[int]) -> int:
         maximum = 0
         for ix, x in enumerate(nums):
@@ -32,46 +32,30 @@ class Solution:
         return maximum
 
     def brute_force_with_pointers(self, nums: List[int]):
-    # if we find a negative number, continue
-    # if we find positive, save index as first_pointer, keep track of sum
-    # if we find subsequent negative number, keep going, update sum
-    # if we find a positive number, and it is bigger than current sum, update first_pointer to this index
-    # if the positive number is smaller, keep going, update sum
-    # at any point, check if sum > maximum. if yes, save index in second_pointer
-    #
-    # you should be done with a single pass
-        max_sum = 0
+        max_sum = -maxsize
         sum = 0
-        negative_step = False
-        # initiate start index
-        for x in nums:
-            if x < 1:
-                continue
-            else:
-                sum = x
-                max_sum = x
-                break
-        # meat and potatoes 
+        # skip zeros, reset sum if sum goes to zero or below
         for x in nums:
             if x == 0:
                 continue
             if x < 0:
                 sum += x
-                negative_step = True
+                if sum > max_sum:
+                    max_sum = sum
+                if sum <= 0:
+                    sum = 0
             else:
-                # check if current value beats the max current sum - in case previous value was negative
-                if x > sum and negative_step:
-                    print(f"{x=} is bugger than {sum=}")
-                    # set new max if x can beat it
-                    if x > max_sum:
-                        print(f"{x=} is bigger than {max_sum=}")
-                        max_sum = x
-                    # set a new start pointer
-                    sum = x
-                    negative_step = False
-                else:
-                    sum += x
-                    if sum > max_sum:
-                        print(f"X is smaller than sum, but sum={sum} is bigger than max sum")
-                        max_sum = sum
+                sum += x
+                if sum > max_sum:
+                    max_sum = sum
         return max_sum
+
+    def brute_force_with_pointers_slim(self, nums: List[int]):
+        maximum = nums[0]
+        sum = 0
+        for n in nums:
+            if sum < 0: # if there is only one element, it will be taken instead of 0
+                sum = 0
+            sum += n
+            maximum = max(sum, maximum)
+        return maximum
--- test.py
+++ test.py
@@ -6,35 +6,66 @@ from maxsubarray import Solution
 def s():
     yield Solution()
 
-@pytest.mark.parametrize("nums, expected",[
-    ([-1, 1, 2, -2], 3),
-    ([-2, -1, 1, 2, -2], 3),
-    ([-2, 1, 1, 2, -2], 4),
-    ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
-])
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([-1, 1, 2, -2], 3),
+        ([-2, -1, 1, 2, -2], 3),
+        ([-2, 1, 1, 2, -2], 4),
+        ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
+    ],
+)
 def test_brute_force(s, nums, expected):
     actual = s.brute_force(nums)
     assert expected == actual
 
-@pytest.mark.parametrize("nums, expected",[
-    ([-1, 1, 2, -2], 3),
-    ([-2, -1, 1, 2, -2], 3),
-    ([-2, 1, 1, 2, -2], 4),
-    ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
-])
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([-1, 1, 2, -2], 3),
+        ([-2, -1, 1, 2, -2], 3),
+        ([-2, 1, 1, 2, -2], 4),
+        ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
+    ],
+)
 def test_brute_force_cache(s, nums, expected):
     actual = s.brute_force_cache(nums)
     assert expected == actual
 
-@pytest.mark.parametrize("nums, expected",[
-    ([-1, 1, 2, -2], 3),
-    ([-2, -1, 1, 2, -2], 3),
-    ([-2, 1, 1, 2, -2], 4),
-    ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
-])
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([-1, 1, 2, -2], 3),
+        ([-2, -1, 1, 2, -2], 3),
+        ([-2, 1, 1, 2, -2], 4),
+        ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
+        ([1, 2, -1, 4], 6),
+        ([-1], -1)
+    ],
+)
 def test_brute_force_with_pointers(s, nums, expected):
     actual = s.brute_force_with_pointers(nums)
     assert expected == actual
+
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([-1, 1, 2, -2], 3),
+        ([-2, -1, 1, 2, -2], 3),
+        ([-2, 1, 1, 2, -2], 4),
+        ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
+        ([1, 2, -1, 4], 6),
+        ([-1], -1)
+    ],
+)
+def brute_force_with_pointers_slim (s, nums, expected):
+    actual = s.brute_force_with_pointers(nums)
+    assert expected == actual
+
 
 # def test_divide_and_conquer(s):
 #
