f446523b1826ae4548ec1981dac7cfb04c5a45e6
Witold Zolnowski <blue3656@gmail.com>
2024-02-06 10:27:32 +0000

    wip

--- group_anagrams.py
+++ group_anagrams.py
@@ -1,3 +1,20 @@
+from typing import List
+
+input = ["eat","tea","tan","ate","nat","bat"]
+expected =  [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+def group_anagrams(strs: List[str]) -> List[List[str]]:
+    grouped = []
+    for x in strs:
+        for y in strs:
+            if x == y:
+                continue
+            if is_anagram(x,y):
+                grouped.append([x,y]) # todo we have to check if we alerady have a group with that anagram
+
+
+assert [[""]] == group_anagrams([""]) 
+
 def is_anagram(first: str, second) -> bool:
     f_dict = {}
     for c in first:
@@ -5,7 +22,6 @@ def is_anagram(first: str, second) -> bool:
             f_dict[c] += 1
         else:
             f_dict[c] = 1
-
     for c in second:
         if c in f_dict:
             f_dict[c] -= 1
@@ -17,6 +33,6 @@ def is_anagram(first: str, second) -> bool:
         return True
     return False
 
-
 assert True == is_anagram("bob", "bbo")
 assert False == is_anagram("bob", "bboo")
+assert True == is_anagram("", "")
