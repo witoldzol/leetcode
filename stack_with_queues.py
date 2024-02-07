4b2542948341e567313bcd22a64837d2cee3a2b9
Witold Zolnowski <blue3656@gmail.com>
2024-02-07 14:08:38 +0000

    wip

--- stack_with_queues.py
+++ stack_with_queues.py
@@ -1,11 +1,25 @@
+from collections import deque
+
 class MyStack:
     def __init__(self):
         self.size = 0
+        self.current = deque()
+        self.temp = deque()
     def push(self, x: int) -> None:
-        pass
+        self.current.append(x)
     def pop(self) -> int:
         pass
     def top(self) -> int:
         pass
     def empty(self) -> bool:
         return self.size == 0
+
+
+# LIFO from two FIFO
+# push 1,2,3
+# queue(1,2,4)
+# move to second 
+# second (1,2) - check if last item ( in temp ), don't push to second queue, just return
+# pop -> 3
+# push 4
+# pop -> 4
