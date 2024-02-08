f7c6ff75c9026582f0af0a638ab251e56b2e0695
Witold Zolnowski <blue3656@gmail.com>
2024-02-08 07:28:55 +0000

    perfect square - wip

--- perfect_squares.py
+++ perfect_squares.py
@@ -1,6 +1,16 @@
+import math
 def perfect_squares(n: int) -> int:
-    import math
+    sq = math.sqrt(n)
+    counter = 0
+    if sq % n == 0:
+        return n // sq
+    else:
+        counter += 1
+        reminder = n - (math.floor(sq) * math.floor(sq))
+        print(f"{reminder=}")
 
+perfect_squares(13)
+    
 
 # assert 3 == perfect_squares(12)
 # assert 2 == perfect_squares(13)
