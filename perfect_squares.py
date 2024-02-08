0ee81639e5275a9e107785d0959493d03270413f
Witold Zolnowski <blue3656@gmail.com>
2024-02-08 07:12:25 +0000

    remove old code

--- perfect_squares.py
+++ perfect_squares.py
@@ -1,17 +1,6 @@
 def perfect_squares(n: int) -> int:
     import math
-    x = math.floor(math.sqrt(n))
-    reminder = None
-    counter = 0
-    if n % x == 0:
-        return n // x
-    else:
-        counter += n // x
-        reminder = n - (counter * x)
-    print(f"{reminder=}")
-    print(x)
 
-perfect_squares(13)
 
 # assert 3 == perfect_squares(12)
 # assert 2 == perfect_squares(13)
