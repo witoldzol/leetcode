a0bd84c4f9c59acf806c9efe411addb6bef6a886
Witold Zolnowski <blue3656@gmail.com>
2024-02-09 10:00:59 +0000

    implement cache

--- perfect_squares.py
+++ perfect_squares.py
@@ -1,6 +1,7 @@
 import math
 
-def solve(n: int):
+cache = [0] * (100)
+def solve(n: int, cache):
     if n == 0:
         return 0
     if n < 0:
@@ -9,11 +10,16 @@ def solve(n: int):
     ans = float('inf')
     for i in range(1, max_range + 1):
         reminder = n - (i*i)
-        s = solve(reminder)
+        if cache[reminder]:
+            s = cache[reminder]
+        else:
+            s = solve(reminder, cache)
+            cache[reminder] = s
         ans = min(ans, 1 + s) 
     return ans
 
 
-assert 1 == solve(1)
-assert 3 == solve(12)
-assert 2 == solve(13)
+assert 1 == solve(1, cache)
+assert 3 == solve(12, cache)
+assert 2 == solve(13, cache)
+assert 2 == solve(130)
