d296a1dddd12ff4a337db9bf9b4a1174741c36e4
Witold Zolnowski <blue3656@gmail.com>
2024-02-09 10:07:45 +0000

    move cache to wrapper function

--- perfect_squares.py
+++ perfect_squares.py
@@ -1,26 +1,28 @@
 import math
 
-def solve(n: int, cache = None):
-    if not cache:
-        cache = [0] * (n + 1)
-    if n == 0:
-        return 0
-    if n < 0:
-        return float('inf')
-    max_range = math.floor(n ** 0.5)
-    ans = float('inf')
-    for i in range(1, max_range + 1):
-        reminder = n - (i*i)
-        if cache[reminder]:
-            s = cache[reminder]
-        else:
-            s = solve(reminder, cache)
-            cache[reminder] = s
-        ans = min(ans, 1 + s) 
-    return ans
+def wrapper(n: int):
+    cache = [0] * (n + 1)
+    def solve(n: int):
+        if n == 0:
+            return 0
+        if n < 0:
+            return float('inf')
+        max_range = math.floor(n ** 0.5)
+        ans = float('inf')
+        for i in range(1, max_range + 1):
+            reminder = n - (i*i)
+            if cache[reminder]:
+                s = cache[reminder]
+            else:
+                s = solve(reminder)
+                cache[reminder] = s
+            ans = min(ans, 1 + s) 
+        return ans
+    return solve(n)
 
-assert 1 == solve(1)
-assert 3 == solve(12)
-assert 2 == solve(13)
-assert 2 == solve(1300)
+
+assert 1 == wrapper(1)
+assert 3 == wrapper(12)
+assert 2 == wrapper(13)
+# assert 2 == solve(1300)
 
