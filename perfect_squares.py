7d62256fefb2e4efbed8a7d44d52571a103753d6
Witold Zolnowski <blue3656@gmail.com>
2024-02-10 12:47:32 +0000

    my recreation - not working

--- perfect_squares.py
+++ perfect_squares.py
@@ -1,68 +1,26 @@
 import math
-
-def wrapper(n: int):
-    cache = [0] * (n + 1)
-    def solve(n: int):
-        if n == 0:
-            return 0
-        if n < 0:
-            return float('inf')
-        max_range = math.floor(n ** 0.5)
-        ans = float('inf')
-        for i in range(1, max_range + 1):
-            reminder = n - (i*i)
-            if cache[reminder]:
-                s = cache[reminder]
-            else:
-                s = solve(reminder)
-                cache[reminder] = s
-            ans = min(ans, 1 + s) 
-        return ans
-    return solve(n)
-
-def iterative(n: int):
-    cache = [float('inf')] * (n + 1)
-    cache[0] = 0
-    mid = math.floor(n ** 0.5)
-    for i in range(n+1):
-        for j in range(1, mid + 1):
-            reminder = i - j * j
-            cache[i] = min(cache[i], 1 + cache[reminder])
-    return cache[n]
-
-
-assert 1 == wrapper(4)
-assert 3 == wrapper(12)
-assert 2 == wrapper(13)
-assert 1 == iterative(4)
-assert 3 == iterative(12)
-assert 2 == iterative(13)
-# assert 2 == solve(1300)
-
-def w(n: int):
-    results = [-1] * n
+def perfect_squares(n: int):
+    results = [0] * (n + 1)
     results[0] = 0
     results[1] = 1
-    def bob(n:int):
-        # what is the base case ?
+    def solve(n):
         if n == 0:
             return 0
-        if n == 1:
-            return 1
         if n < 0:
-            raise Exception('Negative number detected')
-        # greedy way is to get the sq root and check if we have a perfect square
-        sqr = n ** 0.5
-        if sqr % 1 == 0:
-            return 1
-        for i in range(1, n + 1): # if we want to do 12, we go through each n, and calculate MIN number of combinations 
-            # i=1, r = 1
-            # i=2, r = 2
-            # i=3, r= #!/usr/bin/env python3
-            # i=4 r=1
-            for j in range(1, math.floor(sqr)):
+            raise Exception(f'Negative number detected {n}')
+        # for every i between 1 and n ( inclusive )
+        # check for perfect squares ( 1 to floor(sq_root(i))) or i*i <= n
+        for i in range(1, n + 1):
+            sqr = n ** 0.5
+            print(f"{i=}")
+            for j in range(1, math.floor(sqr) + 1): # also inclusive
+                print(f"{j=}")
+                reminder = n - (j * j)
+                if results[j] != 0:
+                    sol = results[j]
+                else:
+                    sol = min(results[j], solve(reminder) + 1)
+                    results[j] = sol
+        return results[n]
 
-def test_w():
-    assert 1 == w(4)
-    assert 3 == w(12)
-    assert 2 == w(13)
+print(perfect_squares(4))
