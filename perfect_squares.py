9acf8e1d3f1085a4304d90978d6c7ce912c8b7e6
Witold Zolnowski <blue3656@gmail.com>
2024-02-11 08:26:31 +0000

    fixed iterative approach

--- perfect_squares.py
+++ perfect_squares.py
@@ -21,6 +21,8 @@ def perfect_squares(n: int):
         return ans
     return solve(n)
 
+# 12 = 1 * 12, 2*2 = 4 * 3, 3*3 = 9, + 1*3 =4
+# we need to find a combination of perfect squares that adds up to the lowest number
 def iterative(n: int):
     results = [float('inf')] * (n + 1)
     results[0] = 0
@@ -28,16 +30,32 @@ def iterative(n: int):
     middle = math.floor(sqr)
     for i in range(1, n + 1):
         for j in range(1, middle + 1):
-            reminder = n - j * j
+            reminder = i - j * j
             results[i] = min(results[i], 1 + results[reminder])
     return results[n]
 
+def numSquares(n: int) -> int:
+    # memo = [float('inf')] * (n + 1)
+    memo = [float('inf')] * (n + 1)
+    memo[0] = 0
 
-print(f"solution = {perfect_squares(12)}")
-assert 3 == perfect_squares(12)
-assert 2 == perfect_squares(13)
-assert 2 == perfect_squares(800)
+    sqr = n ** 0.5
+    middle = math.floor(sqr)
+    for i in range(1, n + 1):
+        j = 1
+        for j in range(1, middle + 1):
+            memo[i] = min(memo[i], 1 + memo[i - j * j])
+            
+    return memo[n]
+
+# print(f"solution = {perfect_squares(12)}")
+# assert 3 == perfect_squares(12)
+# assert 2 == perfect_squares(13)
+# assert 2 == perfect_squares(800)
 assert 3 == iterative(12)
 assert 2 == iterative(13)
 assert 2 == iterative(800)
 
+assert 3 == numSquares(12)
+assert 2 == numSquares(13)
+assert 2 == numSquares(800)
