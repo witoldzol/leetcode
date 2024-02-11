51d3e555fa0fa1c69ebc3be4ea6901bbdaa7b099
Witold Zolnowski <blue3656@gmail.com>
2024-02-11 10:51:02 +0000

    gives wrong answer for bigger datasets

--- array/container_with_most_water_11.py
+++ array/container_with_most_water_11.py
@@ -5,15 +5,17 @@ from typing import List
 input = [1,8,6,2,5,4,8,3,7]
 
 def water(nums: List[int]) -> int:
-    max = 0
+    heights = []
     for ix, x in enumerate(nums):
-        for iy, y in enumerate(nums):
-            if ix==iy:
-                continue
-            max_water = (iy - ix) * min(x,y) 
-            if max_water > max:
-                max = max_water
-    return max
+        heights.append((x,ix))
+    sorted_heights = sorted(heights, key=lambda x: x[0], reverse=True)
+    print(sorted_heights)
+    sorted_width = sorted(heights, key=lambda x: x[1], reverse=True)
+    print(sorted_width)
+    for h in sorted_heights:
+        for w in sorted_width:
+            if h[1] != w[1]:
+                return abs(h[1] - w[1]) * min(h[0],w[0])
 
 print(f"solution = {water(input)}")
 
