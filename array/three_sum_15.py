8096a53b9579855875989aaf544b35861c0ba1dc
Witold Zolnowski <blue3656@gmail.com>
2024-02-13 15:56:04 +0000

    wip

--- array/three_sum_15.py
+++ array/three_sum_15.py
@@ -5,15 +5,38 @@ from typing import List
 input = [-1,0,1,2,-1,-4]
 input = [-1,0,1,2,-1,-4,-2,-3,3,0,4]
 
+# def three_sum(input: List[int]):
+#     if len(input) < 3:
+#         return []
+#     s = sorted(input)
+#     for i in range(len(s)):
+#         if s[i] > 0: # if we start with positive, and we are sorted, then we know that 0 is impossible
+#             break
+#         if i > 0 and s[i] == s[i -1 ]: # no idea what's going on here
+#             continue
+#         low, high = 0, len(s) - 1
+#         while low < high:
+#             sum = s[low] + s[high]
+
 def three_sum(input: List[int]):
-    results = set()
+    d = {}
     for ix, x in enumerate(input):
-        for iy, y in enumerate(input):
-            for iz, z in enumerate(input):
-                if x + y + z == 0 and ix != iy and ix != iz and iy != iz:
-                    x, y ,z = sorted([x,y,z])
-                    results.add((x,y,z))
-    return [list(x) for x in results]
+        d[x] = ix
+    s = sorted(input)
+    low = 0
+    high = len(s) - 1
+    results = []
+    while low < high:
+        sum = s[low] + s[high]
+        if sum > 0:
+            if -sum in d:
+                results.append((s[low], s[high], -sum))
+        elif sum > 0:
+            if sum in d:
+                results.append((s[low], s[high], sum))
+
+
+
 
 print(f"solution {three_sum(input)}")
 expected = [[-4,0,4],[-4,1,3],[-3,-1,4],[-3,0,3],[-3,1,2],[-2,-1,3],[-2,0,2],[-1,-1,2],[-1,0,1]]
