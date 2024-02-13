2325561b401ee2a8174058c6cc596864450fcabb
Witold Zolnowski <blue3656@gmail.com>
2024-02-13 10:52:47 +0000

    three_sum first attempt

--- array/three_sum_15.py
+++ array/three_sum_15.py
@@ -10,5 +10,16 @@ def three_sum(input: List[int]):
 # first pass create hash map of values 
 # second pass look at reminder, then check if we have reminder, if not, geet another value and check for reminder
 # when going 0 to n, calc first diff, say 5 - 0 = -5, so we need reminder of -5, then next one is -8, so now need reminder of 3
+    f = 0
+    s = 0
+    t = 0
+    results = set()
+    for ix, x in enumerate(input):
+        for iy, y in enumerate(input):
+            for iz, z in enumerate(input):
+                if x + y + z == 0 and ix != iy and ix != iz and iy != iz:
+                    x, y ,z = sorted([x,y,z])
+                    results.add((x,y,z))
+    return results
 
-
+print(f"solution {three_sum(input)}")
