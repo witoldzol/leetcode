9e6507fab2fd083884699226301fa25512778655
Witold Zolnowski <blue3656@gmail.com>
2024-02-13 10:53:01 +0000

    clean up

--- array/three_sum_15.py
+++ array/three_sum_15.py
@@ -5,14 +5,6 @@ from typing import List
 input = [-1,0,1,2,-1,-4]
 
 def three_sum(input: List[int]):
-    # brute force - get all permutations & count they all up
-# non brute? calc diff ? - 
-# first pass create hash map of values 
-# second pass look at reminder, then check if we have reminder, if not, geet another value and check for reminder
-# when going 0 to n, calc first diff, say 5 - 0 = -5, so we need reminder of -5, then next one is -8, so now need reminder of 3
-    f = 0
-    s = 0
-    t = 0
     results = set()
     for ix, x in enumerate(input):
         for iy, y in enumerate(input):
