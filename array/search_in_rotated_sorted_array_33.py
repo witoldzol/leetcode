411e13b0f3498c45c992b1f6f143916ca1d0c0f0
Witold Zolnowski <blue3656@gmail.com>
2024-02-16 14:51:24 +0000

    binary search - yet again

--- array/search_in_rotated_sorted_array_33.py
+++ array/search_in_rotated_sorted_array_33.py
@@ -22,6 +22,19 @@ def binary_search(nums: List[int], target: int) -> int:
 
     return -1
 
+def bs(nums: List[int], target: int) -> int:
+    low = 0
+    high = len(nums) - 1
+    while low <= high:
+        middle = (low + high ) // 2
+        if nums[middle] == target:
+            return middle
+        elif nums[middle] > target: # 1,2,3,4 , t=4, m=2
+            high = middle - 1
+        else:
+            low = middle + 1
+    return -1
+
 def solve(nums: List[int], target: int) -> int:
     pass
     # we know that if we split list in half, at least one half will be sorted
@@ -29,3 +42,13 @@ def solve(nums: List[int], target: int) -> int:
 #   if not, split the other list in half again, and recheck
 
 # print(solve(input, 4))
+print(bs(nums, 1))
+print(bs(nums, 2))
+print(bs(nums, 3))
+print(bs(nums, 4))
+print(bs(nums, 5))
+print(bs(nums, 6))
+print(bs(nums, 7))
+print(bs(nums, 8))
+print(bs(nums, 9))
+print(bs(nums, 10))
