e8180ad2fecc5e96b804af457c48ee7b36fcffdf
Witold Zolnowski <blue3656@gmail.com>
2024-02-16 15:46:49 +0000

    add notes on how to tackle the rotated array

--- array/search_in_rotated_sorted_array_33.py
+++ array/search_in_rotated_sorted_array_33.py
@@ -4,10 +4,8 @@ import pudb
 from typing import List
 
 nums = [4,5,6,7,0,1,2]
-nums = [0,1,2,4,5,6,7]
 
 def binary_search(nums: List[int], target: int) -> int:
-    # pu.db
     low = 0
     high = len(nums) - 1
     while low <= high:
@@ -19,36 +17,65 @@ def binary_search(nums: List[int], target: int) -> int:
             high = mid-1
         else:
             low = mid+1
-
     return -1
 
-def bs(nums: List[int], target: int) -> int:
-    low = 0
-    high = len(nums) - 1
+def binary_search(nums: List[int], target: int, low: int = 0, high = 0) -> int:
+    if not high:
+        high = len(nums)
     while low <= high:
-        middle = (low + high ) // 2
-        if nums[middle] == target:
-            return middle
-        elif nums[middle] > target: # 1,2,3,4 , t=4, m=2
-            high = middle - 1
+        mid = (low + high) // 2
+        print(f"{low=} {mid=} {high=}")
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target: # 6, we need 4
+            high = mid-1
         else:
-            low = middle + 1
-    return -1
+            low = mid+1
 
 def solve(nums: List[int], target: int) -> int:
-    pass
     # we know that if we split list in half, at least one half will be sorted
 #   if the sorted list contains the target number, we can just do binary search on it
 #   if not, split the other list in half again, and recheck
+    # split until we find the target inside in sorted half 
+    # split in half
+    middle = len(nums) // 2
+    # figure out if the left side is in order
+    # # LEFT in order
+    # if nums[0] <= nums[middle]:
+    #     # TARGET INSIDE 
+    #     if target <= nums[middle]: # target in left side
+    #         return binary_search(nums, target, 0, middle)
+    # # RIGHT in order
+    # else:
+    #     # TARGET INSIDE 
+    #     if target >= nums[middle]:
+    #         return binary_search(nums[middle:], target, middle, len(nums))
+    while True: 
+        if nums[0] <= nums[middle] and target <= nums[middle]: # target in left side
+                return binary_search(nums, target, 0, middle)
+        elif target >= nums[middle]:
+                return binary_search(nums, target, middle, len(nums))
+        else:
+
+        1) left sorted:
+            left contains -> return
+            left not contains -> drop left, split again
+        2) left not sorted
+            split again
+        3) right sorted:
+            right contains -> return
+            right not contains -> drop right
+        4) right not sorted
+            split again
+
+        # 4,5,6 
+        # 7,0,1,2
+        #
+
+
+
+
+
+
 
 # print(solve(input, 4))
-print(bs(nums, 1))
-print(bs(nums, 2))
-print(bs(nums, 3))
-print(bs(nums, 4))
-print(bs(nums, 5))
-print(bs(nums, 6))
-print(bs(nums, 7))
-print(bs(nums, 8))
-print(bs(nums, 9))
-print(bs(nums, 10))
