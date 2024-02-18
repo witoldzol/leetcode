2232fdd7c6e35dd1168760aab3253ac0776b4d5c
Witold Zolnowski <blue3656@gmail.com>
2024-02-18 07:31:36 +0000

    sqlzoo null 4 to 10

--- sql/null.sql
+++ sql/null.sql
@@ -8,3 +8,27 @@ SELECT teacher.name, dept.name
 -- 3
 select teacher.name, dept.name from teacher
 left join dept on teacher.dept = dept.id
+-- 4
+select teacher.name, dept.name from teacher
+right join dept on teacher.dept = dept.id
+-- 5
+select name, COALESCE(mobile, '07986 444 2266') from teacher
+-- 6
+select teacher.name, coalesce(dept.name, 'None') from teacher 
+left join dept on teacher.dept=dept.id
+-- 7
+select count(teacher.name), count(mobile) from teacher
+-- 8
+select dept.name, count(teacher.name) from teacher
+right join dept on dept.id=teacher.dept
+group by dept.name
+-- 9
+select name, case when teacher.dept=1 or teacher.dept=2 then 'Sci' else 'Art' end as pre from teacher 
+-- 10
+select name, case 
+  when teacher.dept=1 or teacher.dept=2 then 'Sci' 
+  when teacher.dept=3 then 'Art'
+  else 'None'
+end as pre 
+from teacher 
+
