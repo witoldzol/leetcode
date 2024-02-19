c1168c8e35cdcc877d2d44b81dab5b6166345569
Witold Zolnowski <blue3656@gmail.com>
2024-02-19 07:56:03 +0000

    sqlzoo self_join 5

--- sql/self_join.sql
+++ sql/self_join.sql
@@ -2,3 +2,9 @@
 -- 3
 SELECT id, name FROM stops JOIN route ON (stops.id = route.stop)
   WHERE num = 4 and company = 'LRT'
+-- 5
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 and b.stop=149
+
