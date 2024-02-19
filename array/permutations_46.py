101cc1f7d5cd7952a45edb2fdf69aff7d7a82122
Witold Zolnowski <blue3656@gmail.com>
2024-02-19 14:14:16 +0000

    test bob function - reverse list recursively

--- array/permutations_46.py
+++ array/permutations_46.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/permutations/description/
+import pudb
 
 from typing import List
 
@@ -6,11 +7,11 @@ nums = [1,2,3]
 output = [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 
 def per(nums: List[int]) -> List[List[int]]:
+    pu.db
     results = []
     if len(nums) == 1:
         return [nums[:]]
     for i in range(len(nums)):
-        # base condition
         n = nums.pop(0)
         #   generate permutations
         perms = per(nums)
@@ -23,7 +24,50 @@ def per(nums: List[int]) -> List[List[int]]:
         nums.append(n)
     return results
 
-print(per(nums))
-print(f"{output}")
-assert per(nums).sort() == output.sort()
+# def my_per(nums: List[int]) -> List[List[int]]:
+#     # base condition
+#     if len(nums) == 1:
+#         # return a copy of the array
+#         return [nums[:]]
+#     for _ in nums: 
+#         # remove first element
+#         x = nums.pop(0)
+#         # repeat until you get 1 len
+#         subarrs = my_per(nums)
+#         # iterate over sub arrays
+#         for s in subarrs:
+#
+# # 3 iterations
+# nums = [1,2,3]
+# i = 0
+#     x = 1
+#     sub = perm([2,3])
+        # iterate 2 times
+#       i = 0
+#           x = 2
+    #       sub = perm([3])
+#               len == 1
+#               return copy of [3] in a list
+#           sub = [[3]]
+#           for p in sub
+#               [3].append(2) -> [3,2]
+#
+#
 
+# print(per(nums))
+# print(f"{output}")
+# assert per(nums).sort() == output.sort()
+
+
+def bob(nums):
+    res = []
+    if len(nums) == 1:
+        return [nums[:]]
+    x = nums.pop(0)
+    y = bob(nums)
+    for yy in y:
+        yy.append(x)
+    res.extend(y)
+    return res
+
+print(bob(nums))
