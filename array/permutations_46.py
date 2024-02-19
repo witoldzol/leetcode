f8d1aeb9ba413721c71036e7409461c270fd15e9
Witold Zolnowski <blue3656@gmail.com>
2024-02-19 14:46:53 +0000

    remove duplicate code

--- array/permutations_46.py
+++ array/permutations_46.py
@@ -7,7 +7,6 @@ nums = [1,2,3]
 output = [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 
 def per(nums: List[int]) -> List[List[int]]:
-    pu.db
     results = []
     if len(nums) == 1:
         return [nums[:]]
@@ -23,52 +22,3 @@ def per(nums: List[int]) -> List[List[int]]:
         # add n back
         nums.append(n)
     return results
-
-# def my_per(nums: List[int]) -> List[List[int]]:
-#     # base condition
-#     if len(nums) == 1:
-#         # return a copy of the array
-#         return [nums[:]]
-#     for _ in nums: 
-#         # remove first element
-#         x = nums.pop(0)
-#         # repeat until you get 1 len
-#         subarrs = my_per(nums)
-#         # iterate over sub arrays
-#         for s in subarrs:
-#
-# # 3 iterations
-# nums = [1,2,3]
-# i = 0
-#     x = 1
-#     sub = perm([2,3])
-        # iterate 2 times
-#       i = 0
-#           x = 2
-    #       sub = perm([3])
-#               len == 1
-#               return copy of [3] in a list
-#           sub = [[3]]
-#           for p in sub
-#               [3].append(2) -> [3,2]
-#
-#
-
-# print(per(nums))
-# print(f"{output}")
-# assert per(nums).sort() == output.sort()
-
-
-def bob(nums):
-    res = []
-    if len(nums) == 1:
-        return nums[:]
-    x = nums.pop(0)
-    y = bob(nums)
-    y.append(x)
-    # for yy in y:
-    #     yy.append(x)
-    res.extend(y)
-    return res
-
-print(bob(nums))
