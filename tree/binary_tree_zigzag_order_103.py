4a7635612781a714fa0ccb4efe0481fb8e0eb708
Witold Zolnowski <blue3656@gmail.com>
2024-02-21 19:01:24 +0000

    zigzag wip

--- tree/binary_tree_zigzag_order_103.py
+++ tree/binary_tree_zigzag_order_103.py
@@ -1,1 +1,34 @@
 # https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+#
+input = [3,9,20,null,null,15,7]
+expected = [[3],[20,9],[15,7]]
+
+from typing import List
+
+def zig(tree: List[int]) -> List[List[int]]:
+    result = []
+    if len(tree) < 2:
+        return [tree]
+    root = tree.pop(0)
+    result.append([root])
+    direction = 'left'
+    count = 0
+    temp = []
+    for x in tree:
+        if count == 2:
+            temp.append(result)
+            temp = []
+        if direction == 'left':
+            temp.append
+        else:
+            if count == 0:
+                temp = [None, x]
+            else:
+                temp[0] = x
+        count += 1
+
+
+
+
+
+assert zig(input) == expected
