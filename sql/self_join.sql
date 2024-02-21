44939c7ea2970311a17c18afaaec4c8478b547d7
Witold Zolnowski <blue3656@gmail.com>
2024-02-21 09:19:09 +0000

    sqlzoo self_join 10

--- sql/self_join.sql
+++ sql/self_join.sql
@@ -30,4 +30,12 @@ join route b on (a.num = b.num and a.company = b.company)
 join stops on (a.stop = stops.id)
 where a.company = 'LRT' and b.stop = 53
 order by b.num
-
+-- 10
+select r1.num, r1.company, s1.name, r4.num, r4.company from route r1
+join route r2 on ( r1.company = r2.company and r1.num = r2.num)
+join stops s1 on ( r2.stop = s1.id)
+join route r3 on ( s1.id = r3.stop )
+join route r4 on ( r3.num = r4.num and r3.company = r4.company )
+where r1.stop = (select id from stops where name = 'Craiglockhart')
+and r4.stop = (select id from stops where name = 'Lochend')
+order by r1.num, s1.name, r4.num
