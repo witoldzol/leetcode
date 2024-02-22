c6481ab31f8b716f26e73d48bf82e320b96df455
Witold Zolnowski <blue3656@gmail.com>
2024-02-22 13:41:10 +0000

    first draft plus TreeNode class

--- tree/binary_tree_zigzag_order_103.py
+++ tree/binary_tree_zigzag_order_103.py
@@ -1,34 +1,51 @@
 # https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
 #
-input = [3,9,20,null,null,15,7]
+import pudb
+input = [3,9,20,None,None,15,7]
 expected = [[3],[20,9],[15,7]]
 
 from typing import List
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 def zig(tree: List[int]) -> List[List[int]]:
+    # pu.db
     result = []
     if len(tree) < 2:
         return [tree]
     root = tree.pop(0)
     result.append([root])
-    direction = 'left'
+    direction = 'right'
     count = 0
     temp = []
     for x in tree:
+        if x == None:
+            continue
+        # flush after every 2 insertions
         if count == 2:
-            temp.append(result)
+            result.append(temp)
             temp = []
+            count = 0
+            if direction == 'left':
+                direction = 'right'
+            else: 
+                direction = 'left'
+        # insertion 
         if direction == 'left':
-            temp.append
+            temp.append(x)
         else:
             if count == 0:
                 temp = [None, x]
             else:
                 temp[0] = x
         count += 1
+    if count == 2:
+        result.append(temp)
+    return result
 
-
-
-
-
-assert zig(input) == expected
+print(zig(input) )
+print(f"exptected = {expected}")
