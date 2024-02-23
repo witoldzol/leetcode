ba7eaca7beb2bdfe29db5d3290849f9641c9fa4e
Witold Zolnowski <blue3656@gmail.com>
2024-02-23 05:41:43 +0000

    second solution for the second highest salary

--- sql/select_second_highest_salary_176.sql
+++ sql/select_second_highest_salary_176.sql
@@ -5,4 +5,6 @@ limit 1
 offset 1)
 as SecondHighestSalary
 
--- solution 2
+-- solution 2 using self join
+select max(e1.salary) as SecondHighestSalary from employee e1
+join employee e2 on (e1.salary < e2.salary)
