dabc851d8e6536b951ea618c0ec76ab4fa8aac7d
Witold Zolnowski <blue3656@gmail.com>
2024-02-23 05:50:09 +0000

    fix sample input

--- tree/binary_tree_zigzag_order_103.py
+++ tree/binary_tree_zigzag_order_103.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
 #
 import pudb
+from collections import deque
 input = [3,9,20,None,None,15,7]
 expected = [[3],[20,9],[15,7]]
 
@@ -17,43 +18,18 @@ l2 = TreeNode(15, None, None)
 r2 = TreeNode(7, None, None)
 r1 = TreeNode(20, l2, r2)
 l1 = TreeNode(9, None, None)
-root = TreeNode(3, l1,l2)
+root = TreeNode(3, l1, r1)
 
 def zigzag(root: TreeNode) -> List[List[int]]:
-    # bfs
-    result = []
-    from collections import deque
     q = deque()
     q.append(root)
-    direction = 'left'
-    temp = []
     while len(q):
         node = q.popleft()
         print(node.val)
-        result.append(temp)
-        if direction == 'left':
-            if node.left:
-                q.append(node.left)
-                temp.append(node.left.val)
-            if node.right:
-                q.append(node.right)
-                temp.append(node.right.val)
-            result.append(temp)
-            if direction == 'left':
-                direction = 'right'
-            else:
-                direction = 'left'
-        else:
-            if node.right:
-                q.append(node.right)
-                temp.append(node.right.val)
-            if node.left:
-                q.append(node.left)
-                temp.append(node.left.val)
-            if direction == 'left':
-                direction = 'right'
-            else:
-                direction = 'left'
+        if node.left:
+            q.append(node.left)
+        if node.right:
+            q.append(node.right)
 
 
 zigzag(root)
