fea4e5ffb90b985904d9ac5daba68d49bafcfd24
Witold Zolnowski <blue3656@gmail.com>
2024-02-26 07:13:27 +0000

    alternative to previous

--- sql/find_duplicate_emails_182.sql
+++ sql/find_duplicate_emails_182.sql
@@ -2,3 +2,6 @@
 select email as Email from Person
 group by email
 having count(email) > 1
+-- alternative 
+select distinct p1.email from person p1, person p2
+where p1.id <> p2.id and p1.email=p2.email
