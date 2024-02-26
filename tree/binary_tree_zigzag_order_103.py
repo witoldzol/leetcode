d08602f6f43d545cd6dadf1202bd67c31cff0ddb
Witold Zolnowski <blue3656@gmail.com>
2024-02-26 07:48:38 +0000

    second version, extended tests still not passing - we need to handle null nodes

--- tree/binary_tree_zigzag_order_103.py
+++ tree/binary_tree_zigzag_order_103.py
@@ -2,9 +2,10 @@
 #
 import pudb
 from collections import deque
-input = [3,9,20,None,None,15,7]
-expected = [[3],[20,9],[15,7]]
-
+# input = [3,9,20,None,None,15,7]
+# expected = [[3],[20,9],[15,7]]
+input = [1,2,3,4,null,null,5]
+expected = [[1],[3,2],[4,5]]
 from typing import List
 
 class TreeNode:
@@ -20,39 +21,71 @@ r1 = TreeNode(20, l2, r2)
 l1 = TreeNode(9, None, None)
 root = TreeNode(3, l1, r1)
 
+# def zigzag(root: TreeNode) -> List[List[int]]:
+#     if not root:
+#         return []
+#     result = []
+#     q = deque()
+#     q.append(root)
+#     result.append([root.val])
+#     direction = 'left'
+#     temp = []
+#     while len(q):
+#         node = q.popleft()
+#         print(node.val)
+#         if direction == 'right':
+#             if node.left:
+#                 q.append(node.left)
+#                 temp.append(node.left.val)
+#             if node.right:
+#                 q.append(node.right)
+#                 temp.append(node.right.val)
+#             direction = 'left'
+#         else:
+#             if node.right:
+#                 q.append(node.right)
+#                 temp.append(node.right.val)
+#             if node.left:
+#                 q.append(node.left)
+#                 temp.append(node.left.val)
+#             direction = 'right'
+#         if temp:
+#             result.append(temp[:])
+#         temp = []
+#     print(f"result {result}")
+#     return result
+
 def zigzag(root: TreeNode) -> List[List[int]]:
     if not root:
         return []
     result = []
     q = deque()
-    q.append(root)
-    result.append([root.val])
+    q.append([root])
     direction = 'left'
     temp = []
+    # pu.db
     while len(q):
-        node = q.popleft()
-        print(node.val)
-        if direction == 'right':
-            if node.left:
-                q.append(node.left)
-                temp.append(node.left.val)
-            if node.right:
-                q.append(node.right)
-                temp.append(node.right.val)
-            direction = 'left'
-        else:
-            if node.right:
-                q.append(node.right)
-                temp.append(node.right.val)
-            if node.left:
-                q.append(node.left)
-                temp.append(node.left.val)
+        nodes = q.popleft()
+        vals = [n.val for n in nodes]
+        result.append(vals)
+        for node in nodes:
+            if direction == 'right':
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+            else:
+                if node.right:
+                    temp.append(node.right)
+                if node.left:
+                    temp.append(node.left)
+            if temp:
+                q.append(temp[:])
+            temp = []
+        if direction == 'left':
             direction = 'right'
-        if temp:
-            result.append(temp[:])
-        temp = []
-    print(f"result {result}")
+        else:
+            direction = 'left'
     return result
 
-
-zigzag(root)
+print(zigzag(root))
