e54ae1e34aa2cabd775e5bd9126a0d454b2dd937
Witold Zolnowski <blue3656@gmail.com>
2024-02-26 07:33:32 +0000

    zigzag - passes initial tests, fails later on

--- tree/binary_tree_zigzag_order_103.py
+++ tree/binary_tree_zigzag_order_103.py
@@ -21,8 +21,12 @@ l1 = TreeNode(9, None, None)
 root = TreeNode(3, l1, r1)
 
 def zigzag(root: TreeNode) -> List[List[int]]:
+    if not root:
+        return []
+    result = []
     q = deque()
     q.append(root)
+    result.append([root.val])
     direction = 'left'
     temp = []
     while len(q):
@@ -31,15 +35,24 @@ def zigzag(root: TreeNode) -> List[List[int]]:
         if direction == 'right':
             if node.left:
                 q.append(node.left)
+                temp.append(node.left.val)
             if node.right:
                 q.append(node.right)
+                temp.append(node.right.val)
             direction = 'left'
         else:
             if node.right:
                 q.append(node.right)
+                temp.append(node.right.val)
             if node.left:
                 q.append(node.left)
+                temp.append(node.left.val)
             direction = 'right'
+        if temp:
+            result.append(temp[:])
+        temp = []
+    print(f"result {result}")
+    return result
 
 
 zigzag(root)
