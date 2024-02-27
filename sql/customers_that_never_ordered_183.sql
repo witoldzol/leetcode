75eb6445f15d22cd2c742c2d4eba829b9a0730ce
Witold Zolnowski <blue3656@gmail.com>
2024-02-27 08:17:10 +0000

    alternative solution with left join

--- sql/customers_that_never_ordered_183.sql
+++ sql/customers_that_never_ordered_183.sql
@@ -1,3 +1,7 @@
 -- https://leetcode.com/problems/customers-who-never-order/description/
 select name as Customers from Customers
 where Customers.id not in (select customerId from Orders)
+-- alternative solution with left join
+select name as Customers from Customers c
+left join orders o on c.id = o.customerId
+where o.customerId is NULL
