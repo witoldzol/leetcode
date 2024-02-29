9d15b6549c8223d599d9e1376e630d8f97da9c7a
Witold Zolnowski <blue3656@gmail.com>
2024-02-29 07:44:39 +0000

    196 using self join

--- sql/delete_duplicate_emails_196.sql
+++ sql/delete_duplicate_emails_196.sql
@@ -1,3 +1,9 @@
 -- using cross join ( cartesian product )
 delete from Person where id in (select p1.id from Person p1, Person p2
 where p1.email = p2.email and p1.id > p2.id)
+-- using self join
+delete from Person where id in (
+    select p1.id from Person p1
+    join Person p2 on p1.email = p2.email
+    where p1.id > p2.id
+)
