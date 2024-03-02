ad8f283b3e546d531d43b37f2309560f708b8357
Witold Zolnowski <blue3656@gmail.com>
2024-03-02 09:06:45 +0000

    stub out validate tree node - 98

--- tree/validate_binary_search_tree_98.py
+++ tree/validate_binary_search_tree_98.py
@@ -1,1 +1,9 @@
 https://leetcode.com/problems/validate-binary-search-tree/description/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+def validate(root: Optional[TreeNode]) -> bool:
+        
