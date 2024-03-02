171cc25fa28c21661c13a6353956d8fef3d4905d
Witold Zolnowski <blue3656@gmail.com>
2024-03-02 09:08:00 +0000

    sql - aggregate solution - 511

--- sql/first_login_511.sql
+++ sql/first_login_511.sql
@@ -2,3 +2,6 @@ https://leetcode.com/problems/game-play-analysis-i/description/
 -- first attempt using sub-query
 select player_id, event_date as first_login from Activity a1
 where event_date <= all ( select event_date from Activity a2 where a1.player_id = a2.player_id)
+-- using group by
+select player_id, min(event_date) as first_login from Activity
+group by player_id
