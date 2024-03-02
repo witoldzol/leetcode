3bd4c6132a93123f89b6007c59243921fef7f8f2
Witold Zolnowski <blue3656@gmail.com>
2024-03-02 09:06:14 +0000

    sql - first login - 511

---
+++ sql/first_login_511.sql
@@ -0,0 +1,4 @@
+https://leetcode.com/problems/game-play-analysis-i/description/
+-- first attempt using sub-query
+select player_id, event_date as first_login from Activity a1
+where event_date <= all ( select event_date from Activity a2 where a1.player_id = a2.player_id)
