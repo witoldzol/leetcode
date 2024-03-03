1f22bf4770b8cb13d3c8319b16a7ac217a3a909d
Witold Zolnowski <blue3656@gmail.com>
2024-03-03 10:31:52 +0000

    what a mess - work in progress, untangling

--- tree/validate_binary_search_tree_98.py
+++ tree/validate_binary_search_tree_98.py
@@ -11,38 +11,55 @@ r1 = TreeNode(3, None , r2)
 l1 = TreeNode(2, l2, None)
 root = TreeNode(1, l1, r1)
 
+# r2 = TreeNode(11, None, None)
+# l2 = TreeNode(1, None, None)
+# r1 = TreeNode(10, None , r2)
+# l1 = TreeNode(2, l2, None)
+# root = TreeNode(5, l1, r1)
+
 from typing import Optional, List
+prev = [None]
+result = [True]
 
-def inorder(root: Optional[TreeNode], result: List[int] = None,  prev: TreeNode = None) -> int:
-    # in order traversal -> go to the bottom of LEFT, then CENTER, then RIGHT
-    # with each step, value should be higher, if not, we have invalid tree
+def is_valid(root: Optional[TreeNode], prev, result) -> bool:
+    if prev[0] != None:
+        print(f"{prev[0].val=}")
+    if root is None:
+        return
+    is_valid(root.left, prev, result)
+    if prev[0]:
+        print(f"current value is {root.val}, and previous value is = {prev[0].val}")
+    if prev[0] and root.val <= prev[0].val:
+        result[0] = False
+        return
+    prev[0] = root
+    is_valid(root.right, prev, result)
+
+vprev = None
+vresult = [True]
+
+def v(root: Optional[TreeNode], vprev, vresult) -> bool:
+    if vprev != None:
+        print(f"{vprev.val=}")
     if not root:
         return True
-    if not result:
-        result = []
-    if root.left:
-        result = inorder(root.left, result)
-    result.append(root.val)
-    if root.right:
-        result = inorder(root.right, result)
-    return result
+    v(root.left, vprev, vresult)
+    if vprev:
+        print(f"current value is {root.val}, and vprevious value is = {vprev.val}")
+    if vprev and root.val <= vprev.val:
+        vresult[0] = False
+        return
+    vprev = root
+    v(root.right, vprev, vresult)
 
-# print(f"solution is = {inorder(root)}")
-
-def is_valid(root: Optional[TreeNode], prev: int = float('-inf')) -> bool:
-    # in order traversal -> go to the bottom of LEFT, then CENTER, then RIGHT
-    # with each step, value should be higher, if not, we have invalid tree
-    if not root:
-        return True
-    # left
-    if root.left:
-        if not is_valid(root.left, root.val):
-            return False
-    # root
-    if root.val <= prev:
-        return False
-    # right
-    return is_valid(root.right, root.val)
+is_valid(root, prev, result)
+print("==================================================")
+print("==================================================")
+print("==================================================")
+v(root, vprev, vresult)
+print(f"{result[0]=}")
+print(f"{vresult[0]=}")
 
 
-print(f"is_valid == {is_valid(root)}")
+# def v(root: Optional[TreeNode], prev, result) -> bool:
+
