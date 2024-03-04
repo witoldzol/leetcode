8b3fcd6da405441bbacab46473420360ee546120
Witold Zolnowski <blue3656@gmail.com>
2024-03-04 07:33:41 +0000

    validate binary tree - done

--- tree/validate_binary_search_tree_98.py
+++ tree/validate_binary_search_tree_98.py
@@ -1,7 +1,5 @@
 # https://leetcode.com/problems/validate-binary-search-tree/description/
-from typing import Optional, List
-# import pudb; pudb.set_trace()
-# import pubd
+from typing import Optional
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -23,47 +21,21 @@ def get_true_tree():
     l1 = TreeNode(2, l2, None)
     return TreeNode(5, l1, r1)
 
-prev = [None]
-result = [True]
+def vv(root: Optional[TreeNode]) -> bool:
+    p = float('-inf')
+    result = True
+    def v(root: Optional[TreeNode]) -> bool:
+        nonlocal p
+        nonlocal result
+        if not root:
+            return True
+        v(root.left)
+        if p > root.val:
+            result = False
+        p = root.val
+        v(root.right)
+        return result
+    return v(root)
 
-def is_valid(root: Optional[TreeNode], prev, result) -> bool:
-    if prev[0] != None:
-        print(f"{prev[0].val=}")
-    if root is None:
-        return
-    is_valid(root.left, prev, result)
-    if prev[0]:
-        print(f"current value is {root.val}, and previous value is = {prev[0].val}")
-    if prev[0] and root.val <= prev[0].val:
-        result[0] = False
-        return
-    prev[0] = root
-    is_valid(root.right, prev, result)
-
-def test_is_valid():
-    global prev
-    global result
-    is_valid(get_true_tree(), prev, result)
-    assert True == result[0]
-    prev = [None]
-    result = [True]
-    is_valid(get_false_tree(), prev, result)
-    assert False == result[0]
-    print("================================================================================")
-    print("================================================================================")
-    print("================================================================================")
-
-def v(root: Optional[TreeNode], prev: int = float('-inf')) -> bool:
-    # pu.db
-    if not root:
-        return None
-    v(root.left, root.val)
-    if prev and root.val < prev:
-        return False
-    print(f"visiting node = {root.val}, previous node = {prev}")
-    r = v(root.right, root.val)
-    if r == False:
-        return False
-    return True
-
-print(f"result = {v(get_true_tree())}")
+print(f"result = {vv(get_true_tree())}")
+print(f"result = {vv(get_false_tree())}")
