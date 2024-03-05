0e0ba05475e042369ae974b561323aa03060f347
Witold Zolnowski <blue3656@gmail.com>
2024-03-05 22:38:09 +0000

    ll problem 02 - stub out main function

---
+++ linked-list/add_two_numbers_2.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/add-two-numbers/description/
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+from typing import Optional
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        return None
