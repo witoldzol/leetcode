98dd826884aebddb75249a986dfd54312e7b26fb
Witold Zolnowski <blue3656@gmail.com>
2024-03-05 09:13:47 +0000

    sql 586 first solution

---
+++ sql/customer_with_highest_order_number_586.sql
@@ -0,0 +1,5 @@
+-- https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/description/
+select customer_number from Orders
+group by customer_number
+order by count(order_number) desc
+limit 1
