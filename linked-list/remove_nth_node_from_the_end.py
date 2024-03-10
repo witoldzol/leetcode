384b460648782c3e64d1325d9bcaff09198cb7d7
Witold Zolnowski <blue3656@gmail.com>
2024-03-10 08:49:36 +0000

    delete nth node - working

--- linked-list/remove_nth_node_from_the_end.py
+++ linked-list/remove_nth_node_from_the_end.py
@@ -8,9 +8,14 @@ def removeNthFromEnd(head: Optional[ListNode], n: int) -> Optional[ListNode]:
 # len - n - 1 node.next = node.next.next # done
     length = 0
     curr = head
+    # calc len
     while curr:
         length += 1
         curr = curr.next
+    # edge case
+    if length == n:
+        head = head.next
+        return head
     node_before_node_to_be_deleted = length - n - 1
     curr = head
     for _ in range(node_before_node_to_be_deleted):
@@ -19,29 +24,18 @@ def removeNthFromEnd(head: Optional[ListNode], n: int) -> Optional[ListNode]:
     curr.next = curr.next.next
     return head
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# TESTS
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-h = array_to_linked_list([1,2,3])
-result = removeNthFromEnd(h, 2)
-expected = [1,3]
-try:
+def test_bob():
+    h = array_to_linked_list([1,2,3])
+    result = removeNthFromEnd(h, 2)
+    expected = [1,3]
     assert expected == ll_to_array(result)
-except:
-    print(f"ERROR:\n{result=} vs {expected=}")
 
-h = array_to_linked_list([1,2,3,4,5])
-result = removeNthFromEnd(h, 2)
-expected = [1,2,3,5]
-try:
+    h = array_to_linked_list([1,2,3,4,5])
+    result = removeNthFromEnd(h, 2)
+    expected = [1,2,3,5]
     assert expected == ll_to_array(result)
-except:
-    print(f"ERROR:\n{result=} vs {expected=}")
 
-h = array_to_linked_list([1,2])
-result = removeNthFromEnd(h, 2)
-expected = [2]
-try:
+    h = array_to_linked_list([1,2])
+    result = removeNthFromEnd(h, 2)
+    expected = [2]
     assert expected == ll_to_array(result)
-except:
-    print(f"ERROR:\n{result=} vs {expected=}")
