7309a5751a66278faa3ddf8036af27b17f35e201
Witold Zolnowski <blue3656@gmail.com>
2024-03-10 07:22:34 +0000

    remove nth node from the end of ll - stub out

---
+++ linked-list/remove_nth_node_from_the_end.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def removeNthFromEnd(head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    pass
+
