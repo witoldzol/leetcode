3fc0e1d9c4e2ab2419e7a83ee3b3d6053f97a41a
Witold Zolnowski <blue3656@gmail.com>
2024-03-11 07:15:42 +0000

    sql 619

---
+++ sql/select_highest_number_619.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/biggest-single-number/description/
+select max(num) as num from MyNumbers where num in (
+  select num from MyNumbers
+  group by num
+  having count(num) = 1
+)
