05dc7c899a9b9cfb7652e6ddd8c5861ff2ac2008
Witold Zolnowski <blue3656@gmail.com>
2024-03-14 07:30:23 +0000

    sql 627 - non enum value example

--- sql/update_salary_627.sql
+++ sql/update_salary_627.sql
@@ -1,6 +1,14 @@
 -- https://leetcode.com/problems/swap-salary/description/
 update salary set sex = 
-case sex
+case sex -- this works only because sex is an ENUM!
     when 'm' then 'f'
     else 'm'
 end;
+-- example with a non-enum value
+update Salary
+set salary = 
+case 
+when salary < 5000 then salary * 1.1
+else salary
+end;
+
