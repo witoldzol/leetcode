eae9be3d296df4c7e627566896a9547729e751bb
Witold Zolnowski <blue3656@gmail.com>
2024-03-18 07:34:40 +0000

    zigzag - passing first test

--- array/zigzag_conversion_6.py
+++ array/zigzag_conversion_6.py
@@ -1,11 +1,34 @@
 # https://leetcode.com/problems/zigzag-conversion/
+from typing import Tuple
+
+def get_counter(curr: int, max: int, direction: int) -> Tuple[int, int]:
+    max -= 1
+    if curr == -1:
+        print(f"got -1, returning curr = 0")
+        return 0, 1
+    if curr == max:
+        direction = -1
+    if curr == 0:
+        direction = 1
+    return curr + direction, direction
 
 def convert(s: str, numRows: int) -> str:
     ars = {}
     for i in range(numRows):
-        ars[f"array{i}"] = []
-    for a in ars:
-        print(a)
+        ars[i] = []
+    print(ars)
+    direction = 1
+    counter = -1
+    for char in s:
+        print(f"{numRows=}")
+        counter, direction = get_counter(counter, numRows, direction)
+        print(f"im in loop, curr is {counter}")
+        ars[counter].append(char)
+    result = ""
+    for v in ars.values():
+        result += (''.join(v))
+    return result
 
-convert("BOB", 4)
+result = convert("PAYPALISHIRING",3)
+assert "PAHNAPLSIIGYIR" == result
 
