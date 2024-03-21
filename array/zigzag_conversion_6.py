33fbeb4a9f38d6db1d2c42669843552fff237547
Witold Zolnowski <blue3656@gmail.com>
2024-03-21 06:53:24 +0000

    zigzag - solved

--- array/zigzag_conversion_6.py
+++ array/zigzag_conversion_6.py
@@ -4,7 +4,6 @@ from typing import Tuple
 def get_counter(curr: int, max: int, direction: int) -> Tuple[int, int]:
     max -= 1
     if curr == -1:
-        print(f"got -1, returning curr = 0")
         return 0, 1
     if curr == max:
         direction = -1
@@ -13,16 +12,15 @@ def get_counter(curr: int, max: int, direction: int) -> Tuple[int, int]:
     return curr + direction, direction
 
 def convert(s: str, numRows: int) -> str:
+    if numRows == 1:
+        return s
     ars = {}
     for i in range(numRows):
         ars[i] = []
-    print(ars)
     direction = 1
     counter = -1
     for char in s:
-        print(f"{numRows=}")
         counter, direction = get_counter(counter, numRows, direction)
-        print(f"im in loop, curr is {counter}")
         ars[counter].append(char)
     result = ""
     for v in ars.values():
@@ -31,4 +29,7 @@ def convert(s: str, numRows: int) -> str:
 
 result = convert("PAYPALISHIRING",3)
 assert "PAHNAPLSIIGYIR" == result
+
+result = convert("AB",1)
+assert "AB" == result
 
