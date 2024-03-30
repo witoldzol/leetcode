52aa592ba989587e33c118d6fcd72c6e3434a23b
Witold Zolnowski <blue3656@gmail.com>
2024-03-30 06:40:15 +0000

    sql 1527 - add pandas

--- sql/patients_with_conditions_1527.sql
+++ sql/patients_with_conditions_1527.sql
@@ -1,2 +1,9 @@
 select * from Patients
 where conditions ~ '^DIAB1.*| DIAB1.*
+
+-- alternative with a crazy lookaround ( postgress has posix compliant regex, hence doesn't support \b )
+select * from Patients
+where conditions ~ '(?<=^|[^a-zA-Z0-9_])DIAB1\w+'
+
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    return patients[patients['conditions'].str.contains(r'\bDIAB1')]
