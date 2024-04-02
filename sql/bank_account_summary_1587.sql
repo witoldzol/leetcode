f01ad18500f205ded39c748265c0fdd5dd6c9738
Witold Zolnowski <blue3656@gmail.com>
2024-04-02 06:01:23 +0100

    pandas 1587

--- sql/bank_account_summary_1587.sql
+++ sql/bank_account_summary_1587.sql
@@ -6,3 +6,10 @@ having sum(amount) > 10000
 
 
 -- pandas
+def account_summary(users: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
+   AMOUNT = 10_000 
+   df = transactions.groupby(['account'])['amount'].sum().reset_index()
+   filtered = df[df['amount'] > AMOUNT]
+   df = filtered.merge(users, on='account')
+   df = df[['name', 'amount']]
+   return df.rename(columns={'name': 'NAME', 'amount': 'BALANCE'})
