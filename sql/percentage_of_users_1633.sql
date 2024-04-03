dedc8f3c5c3e38319d93f4bc243c31ff23f6e09c
Witold Zolnowski <blue3656@gmail.com>
2024-04-03 06:40:02 +0100

    pandas 1633

--- sql/percentage_of_users_1633.sql
+++ sql/percentage_of_users_1633.sql
@@ -6,3 +6,12 @@ round(
 ) * 100 as percentage from Register r
 group by r.contest_id
 order by percentage desc, r.contest_id asc
+
+
+-- pandas
+def users_percentage(users: pd.DataFrame, register: pd.DataFrame) -> pd.DataFrame:
+    user_count = len(users) * 1.0
+    df = register.groupby('contest_id')['user_id'].count().reset_index()
+    df['percentage'] = round((df['user_id']/user_count) * 100, 2)
+    df = df.sort_values(by=['percentage', 'contest_id'], ascending=[False, True])
+    return df[['contest_id','percentage']]
