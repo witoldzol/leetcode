6e83da2af81e18bfe0d5cfc6699107cb826c5b0b
Witold Zolnowski <blue3656@gmail.com>
2024-04-04 21:01:33 +0100

    pandas 1517

--- sql/valid_email_1517.sql
+++ sql/valid_email_1517.sql
@@ -1,3 +1,8 @@
 -- https://leetcode.com/problems/find-users-with-valid-e-mails/description/
 select * from Users
 where mail ~ '^[[:alpha:]]+[A-Za-z0-9\-\.\_]*@leetcode\.com$'
+
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    pattern = r'^[a-zA-Z]+[a-zA-Z0-9\.\-_]*@leetcode\.com$'
+    filtered = users[users['mail'].str.match(pattern)]
+    return filtered
