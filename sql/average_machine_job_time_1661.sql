f029d8af40eda8a8420a524a6d3d5073fdcc10c5
Witold Zolnowski <blue3656@gmail.com>
2024-04-05 08:12:02 +0100

    sql 1661

---
+++ sql/average_machine_job_time_1661.sql
@@ -0,0 +1,12 @@
+-- https://leetcode.com/problems/average-time-of-process-per-machine/description/
+
+-- notice the cast to numeric value inside the round function
+select a.machine_id,
+round(avg((b.timestamp - a.timestamp)::numeric), 3) as processing_time from Activity a
+join Activity b on a.machine_id = b.machine_id 
+and a.process_id = b.process_id
+and a.activity_type = 'start'
+and b.activity_type = 'end'
+group by a.machine_id
+
+-- instead of casting lets use a function
