bd28c02c11c5ca77673d1e57beef6dd82555b00b
Witold Zolnowski <blue3656@gmail.com>
2024-04-05 08:12:27 +0100

    plpgsql 1661

--- sql/average_machine_job_time_1661.sql
+++ sql/average_machine_job_time_1661.sql
@@ -10,3 +10,18 @@ and b.activity_type = 'end'
 group by a.machine_id
 
 -- instead of casting lets use a function
+create or replace function round_avg(value numeric, decimal_places integer)
+returns numeric as $$
+begin
+    return round(value, decimal_places);
+end;
+$$ language plpgsql;
+
+select a.machine_id,
+round_avg((b.timestamp - a.timestamp), 3) as processing_time 
+from Activity a
+join Activity b on a.machine_id = b.machine_id 
+and a.process_id = b.process_id
+and a.activity_type = 'start'
+and b.activity_type = 'end'
+group by a.machine_id
