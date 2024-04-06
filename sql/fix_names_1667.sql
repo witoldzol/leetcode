a2f9f1df0eaac62075f0be5c7f5311fa956a62da
Witold Zolnowski <blue3656@gmail.com>
2024-04-06 08:18:26 +0100

    pandas 1667

--- sql/fix_names_1667.sql
+++ sql/fix_names_1667.sql
@@ -1,3 +1,8 @@
 -- https://leetcode.com/problems/fix-names-in-a-table/description/
 select user_id, upper(left(name, 1)) || substring(lower(name) from 2) as name from Users
 order by user_id
+
+
+def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    users['name'] = users['name'].str.capitalize()
+    return users.sort_values(by='user_id')
