21afe1135a6dda1aea7747ec56f4fee8bedd062d
Witold Zolnowski <blue3656@gmail.com>
2024-04-07 07:00:37 +0100

    pandas 1683

--- sql/valid_tweets_1683.sql
+++ sql/valid_tweets_1683.sql
@@ -1,3 +1,8 @@
 -- https://leetcode.com/problems/invalid-tweets/description/
 select tweet_id from Tweets
 where length(content) > 15
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    fil = tweets[tweets['content'].str.len() > 15]
+    return fil['tweet_id'].to_frame()
+    
