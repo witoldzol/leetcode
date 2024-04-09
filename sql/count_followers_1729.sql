63835235b4eeb42d772b8bbaa08d70d1695c8f78
Witold Zolnowski <blue3656@gmail.com>
2024-04-09 07:37:28 +0100

    pandas 1729

--- sql/count_followers_1729.sql
+++ sql/count_followers_1729.sql
@@ -5,4 +5,9 @@ group by user_id
 order by user_id asc
 
 
-
+def count_followers(followers: pd.DataFrame) -> pd.DataFrame:
+    df = followers.groupby('user_id')['follower_id'].count().reset_index()
+    df.rename(columns={'follower_id': 'followers_count'}, inplace=True)
+    df.sort_values(by='user_id', ascending=True)
+    return df
+    
