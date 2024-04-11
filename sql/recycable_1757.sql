89f08734b1274ba087baa1353e75b79e969c3f63
Witold Zolnowski <blue3656@gmail.com>
2024-04-11 06:33:43 +0100

    pandas 1757

--- sql/recycable_1757.sql
+++ sql/recycable_1757.sql
@@ -1,3 +1,8 @@
 -- https://leetcode.com/problems/recyclable-and-low-fat-products/
 select product_id from Products
 where low_fats='Y' and recyclable='Y'
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    df = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
+    return df['product_id'].to_frame()
+
