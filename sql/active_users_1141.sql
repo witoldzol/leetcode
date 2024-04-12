97b4814f39bdc34ca7100eebca3c8923e7e63879
Witold Zolnowski <blue3656@gmail.com>
2024-04-12 06:19:45 +0100

    fix sql 1141

--- sql/active_users_1141.sql
+++ sql/active_users_1141.sql
@@ -1,4 +1,6 @@
 -- https://leetcode.com/problems/user-activity-for-the-past-30-days-i/description/
-select activity_date as day, count(distinct user_id) as active_users from Activity
-where activity_date between TO_DATE('2019-07-27', 'YYYY-MM-DD') - INTERVAL '30 days' and '2019-07-27'
+select activity_date as day,
+count(distinct user_id) as active_users 
+from Activity
+where activity_date > DATE '2019-07-27' - 30 and activity_date <= DATE '2019-07-27'
 group by activity_date
