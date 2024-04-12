16bd7fca80c7e697c8cd01ce35fbee7f913b2b77
Witold Zolnowski <blue3656@gmail.com>
2024-04-12 07:04:56 +0100

    pandas 1731

--- sql/number_of_reportees_1731.sql
+++ sql/number_of_reportees_1731.sql
@@ -4,4 +4,12 @@ where activity_date between TO_DATE('2019-07-27', 'YYYY-MM-DD') - INTERVAL '30 d
 group by activity_date
 
 
-
+import pandas as pd
+def count_employees(employees: pd.DataFrame) -> pd.DataFrame:
+    df = employees.groupby('reports_to', as_index=False).agg(reports_count=('name', 'count'), average_age=('age', 'mean'))
+    df = df.merge(employees, how='left', left_on='reports_to', right_on='employee_id')
+    df = df[['reports_to_x', 'name', 'reports_count', 'average_age']]
+    df = df.rename(columns={'reports_to_x': 'employee_id'})
+    df['average_age'] = round(df['average_age'] + 0.1)
+    return df
+    
