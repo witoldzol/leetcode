61cf21e87eae3524282a768085192b610be115a8
Witold Zolnowski <blue3656@gmail.com>
2024-04-13 22:14:56 +0100

    pandas 1741

--- sql/employee_time_in_office_1741.sql
+++ sql/employee_time_in_office_1741.sql
@@ -4,3 +4,9 @@ emp_id,
 sum(out_time - in_time) as total_time
 from Employees
 group by emp_id, event_day
+
+def total_time(employees: pd.DataFrame) -> pd.DataFrame:
+    df = employees.groupby(['emp_id','event_day'])[['in_time','out_time']].sum().reset_index()
+    df['total_time'] = df['out_time'] - df['in_time']
+    df.rename(columns={'event_day': 'day'}, inplace=True)
+    return df[['day','emp_id','total_time']]
