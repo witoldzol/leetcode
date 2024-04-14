503bb7ef62bf368ac27c4b081d71bfa3ef88ddcf
Witold Zolnowski <blue3656@gmail.com>
2024-04-14 07:20:47 +0100

    sql 1789

---
+++ sql/primary_department_1789.sql
@@ -0,0 +1,12 @@
+-- https://leetcode.com/problems/primary-department-for-each-employee/description/
+select employee_id, department_id
+from Employee
+where primary_flag='Y' 
+or employee_id in
+(   select employee_id from Employee 
+    group by employee_id 
+    having count(department_id) = 1 
+)
+
+
+
