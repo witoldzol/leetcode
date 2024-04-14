0bb0502ca1a934bd9737a3f989272e0704af66e3
Witold Zolnowski <blue3656@gmail.com>
2024-04-14 04:20:51 +0100

    dop - add is_vip_member, add_book_item

--- dop/library.py
+++ dop/library.py
@@ -43,10 +43,17 @@ class UserManagement:
     def is_super_member(user_data: UserData, user_id: str):
         pass
 
+    @staticmethod
+    def is_vip_member(user_data: UserData, user_id: str):
+        pass
 
 class Catalog:
     @staticmethod
     def get_book_lendings(catalog_data: CatalogData, user_id: str):
+        pass
+
+    @staticmethod
+    def add_book_item(catalog_data: CatalogData, book_item_info: BookItemInfo):
         pass
 
 class Library:
@@ -61,6 +68,14 @@ class Library:
     @staticmethod
     def search_book(library_data: LibraryData, search_query: str):
         pass
+
+    @staticmethod
+    def add_book_item(library_data: LibraryData,
+                      user_id: str,
+                      book_item_info: BookItemInfo):
+        if UserManagement.is_librarian(library_data.user_data, user_id) or \
+           UserManagement.is_super_member(library_data.user_data, user_id):
+            Catalog.add_book_item(library_data.catalog_data, book_item_info)
 
 # OTHER STUFF
 class CatalogCode:
