cd32d4d5420e3def8600a5241f7e0589d8735cb7
Witold Zolnowski <blue3656@gmail.com>
2024-04-14 18:06:08 +0100

    dop - add watchmen sample record

--- dop/library.py
+++ dop/library.py
@@ -1,4 +1,23 @@
 from typing import List, Dict
+### sample ###
+watchment = {
+  "isbn": "978-1779501127",
+  "title": "Watchmen",
+  "publicationYear": 1987,
+  "authors": ["alan-moore", "dave-gibbons"],
+  "bookItems": [
+    {
+      "id": "book-item-1",
+      "libId": "nyc-central-lib",
+      "isLent": True
+    },
+    {
+      "id": "book-item-2",
+      "libId": "nyc-central-lib",
+      "isLent": False
+    }
+  ]
+}
 ###################################### DATA ##############################################################
 
 class BookLendingData:
