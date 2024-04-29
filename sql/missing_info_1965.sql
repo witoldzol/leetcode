c41f0f2ca90b4d8cd8b5b8f3b63422c5f73ee7c7
Witold Zolnowski <witold.zolnowski@renre.com>
2024-04-29 08:54:44 +0100

    pandas 1965

--- sql/missing_info_1965.sql
+++ sql/missing_info_1965.sql
@@ -6,3 +6,11 @@ select t.employee_id from
 ) t
 where t.name is null or t.salary is null
 order by t.employee_id asc
+
+def find_employees(employees: pd.DataFrame, salaries: pd.DataFrame) -> pd.DataFrame:
+    df = employees
+    df = df.merge(salaries, on='employee_id', how='outer')
+    filtered = df[df['name'].isnull() | df['salary'].isnull()]
+    ids = filtered['employee_id'].to_frame()
+    sorted = ids.sort_values(by='employee_id', ascending=True)
+    return sorted
