ad964d5061bf7721819b2ea9373181501a3b2d1c
Witold Zolnowski <blue3656@gmail.com>
2024-05-10 08:02:36 +0100

    pandas alternative 178

--- sql/178.sql
+++ sql/178.sql
@@ -10,3 +10,26 @@ def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
     # rank the scores
     df['rank'] = df['score'].rank(method='dense', ascending=False)
     return df.sort_values(by='score', ascending=False)[['score','rank']]
+
+-- my pandas solution
+import pandas as pd
+rank: int = 1
+current_score = None
+
+def rank_rows(row):
+    global rank
+    global current_score
+    if current_score is None:
+        current_score = row['score']
+    if row['score'] == current_score:
+        return rank
+    else:
+        current_score = row['score']
+        rank = rank + 1
+        return rank
+
+def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+    df = scores
+    df = df.sort_values(by='score', ascending=False)
+    df['rank'] = df.apply(rank_rows, axis=1)
+    return df[['score', 'rank']]
