010ef054af45d314407e1799234b7b6f0911cf7c
Witold Zolnowski <blue3656@gmail.com>
2025-04-14 07:25:55 +0100

    plus one

--- array/plus_one.py
+++ array/plus_one.py
@@ -1,20 +1,25 @@
-# input = [1,2,3]
-# expected = [1,2,4]
+input = [1,2,3]
 # input = [1,9]
-# expected = [2,0]
 input = [9]
-expected = [1,0]
 
 def plus_one(input):
-    for i in range(len(input)):
-        idx = len(input) - 1 - i
-        input[idx] += 1
-        if input[idx] == 10:
-            input[idx]= 0
+    # we loop over every item in array in reverse order
+    # we increment last item by one
+    # then we check it, if it's 10
+    # we change it to 0 and continue 
+    # if not 10, break loop and exit
+    
+    # if we went throught the entire loop
+    # that means last item also was 10
+    # which means we need to append [1] to the original array
+    for i in range(len(input) -1, -1, -1):
+        input[i] += 1
+        if input[i] == 10:
+            input[i] = 0
         else:
             return input
-    return [1] + input
+        return [1] + input
 
-print(input)
 print(plus_one(input))
+
 
